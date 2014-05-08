5d8b683215083f54533f2c581acaf37b831040ff
Jonas Bieri <jonas.bieri@gmail.com>
2014-05-08 14:20:28 +0200

    changed box type in plot 3

--- plot4.R
+++ plot4.R
@@ -16,7 +16,7 @@ plot(data$DateTime, data$Global_active_power, type="l", xlab="", ylab="Global Ac
 plot(data$DateTime, data$Sub_metering_1, type="l", col="black", xlab="", ylab="Enegry sub metering")
 lines(data$DateTime, data$Sub_metering_2, col="red")
 lines(data$DateTime, data$Sub_metering_3, col="blue")
-legend("topright", legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), lty=1, col=c("black", "red", "blue"))
+legend("topright", legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), lty=1, col=c("black", "red", "blue"), bty="n")
 
 plot(data$DateTime, data$Voltage, type="l", xlab="datetime")
 
